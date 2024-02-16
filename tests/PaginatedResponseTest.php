d8e615b9b901ef4cef2438e4e6080d5a5a68e011
Stefanius <sgrootveld@gmail.com>
2024-02-16 12:37:43 +0100

    Add test coverage

--- tests/PaginatedResponseTest.php
+++ tests/PaginatedResponseTest.php
@@ -8,16 +8,18 @@ use TestMonitor\Clickup\Responses\PaginatedResponse;
 class PaginatedResponseTest extends TestCase
 {
     /** @test */
-    public function it_should_has_a_default_page_limit_of_100()
+    public function it_should_return_a_paginated_response()
     {
         // Given
-        $repsonse = new PaginatedResponse([0 => 'Foo', 1 => 'Bar']);
+        $repsonse = new PaginatedResponse([0 => 'Foo', 1 => 'Bar'], true);
 
         // When
-        $result = $repsonse->perPage();
+        $result = $repsonse->items();
 
         // Then
-        $this->assertEquals(100, $result);
+        $this->assertCount(2, $result);
+        $this->assertIsArray( $result);
+        $this->assertEquals( 'Foo', $result[0]);
     }
 
     /** @test */
@@ -35,7 +37,20 @@ class PaginatedResponseTest extends TestCase
     }
 
     /** @test */
-    public function it_should_determines_there_are_more_items()
+    public function it_should_have_a_fixed_page_limit_of_100_items()
+    {
+        // Given
+        $repsonse = new PaginatedResponse([0 => 'Foo', 1 => 'Bar']);
+
+        // When
+        $result = $repsonse->perPage();
+
+        // Then
+        $this->assertEquals(100, $result);
+    }
+
+    /** @test */
+    public function it_should_be_able_to_determine_that_there_are_more_items()
     {
         // Given
         $repsonse = new PaginatedResponse([0 => 'Foo', 1 => 'Bar'], true);
