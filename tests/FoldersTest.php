08f5ca3fe33a5887b3c61d7765a19c5e7e6f18c7
Frank Keulen <frank@keulen-musson.nl>
2024-05-01 14:24:38 +0200

    Fixed tests

--- tests/FoldersTest.php
+++ tests/FoldersTest.php
@@ -92,7 +92,7 @@ class FoldersTest extends TestCase
 
         $service->shouldReceive('request')->once()->andReturn($response = Mockery::mock('Psr\Http\Message\ResponseInterface'));
         $response->shouldReceive('getStatusCode')->andReturn(404);
-        $response->shouldReceive('getBody')->andReturnNull();
+        $response->shouldReceive('getBody')->andReturn(\GuzzleHttp\Psr7\Utils::streamFor());
 
         $this->expectException(NotFoundException::class);
 
--- tests/TagsTest.php
+++ tests/TagsTest.php
@@ -89,7 +89,7 @@ class TagsTest extends TestCase
 
         $service->shouldReceive('request')->once()->andReturn($response = Mockery::mock('Psr\Http\Message\ResponseInterface'));
         $response->shouldReceive('getStatusCode')->andReturn(404);
-        $response->shouldReceive('getBody')->andReturnNull();
+        $response->shouldReceive('getBody')->andReturn(\GuzzleHttp\Psr7\Utils::streamFor());
 
         $this->expectException(NotFoundException::class);
 
