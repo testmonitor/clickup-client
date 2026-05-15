ef7878baf468f07bfacf9db0d96accfdb50b72be
Stephan Grootveld <stephan.grootveld@cerios.com>
2026-05-15 11:51:01 +0200

    StyleCI fix

--- src/Client.php
+++ src/Client.php
@@ -51,8 +51,8 @@ class Client
      */
     public function __construct(
         array $credentials,
-        AccessToken $token = null,
-        GenericProvider $provider = null
+        ?AccessToken $token = null,
+        ?GenericProvider $provider = null
     ) {
         $this->token = $token;
 
